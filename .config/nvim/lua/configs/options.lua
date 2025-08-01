1a410f88e38de57c4c9b2febfb4eac00539b1031
syxm1 <hisyamprsetyo@gmail.com>
2025-08-01 08:05:53 +0700

    minimalist plugins

--- .config/nvim/lua/configs/options.lua
+++ .config/nvim/lua/configs/options.lua
@@ -1,27 +1,59 @@
-local opt = vim.opt
-local cmd = vim.cmd
+vim.cmd("filetype plugin indent on")
+vim.cmd("syntax enable")
 
-cmd("filetype plugin indent on")
-cmd("syntax enable")
+vim.opt.compatible = false
+vim.opt.nu = true
 
-opt.compatible = false
-opt.nu = true
+vim.opt.backup = false
 
-opt.backup = false
-opt.swapfile = false
+-- basic
+vim.o.signcolumn = "yes"
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.o.wrap = false
+vim.o.scrolloff = 5
+vim.o.sidescrolloff = 10
+vim.o.swapfile = false
 
--- Tabs
-opt.tabstop = 2
-opt.expandtab = true
-opt.smarttab = true
-opt.shiftround = true
-opt.shiftwidth = 0
-opt.softtabstop = 0
+-- indentation
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.autoindent = true
 
-opt.termguicolors = true
-opt.wrap = false
-opt.showmode = false
-opt.showmatch = true
-opt.hlsearch = true
+-- searching
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.hlsearch = true
+vim.o.incsearch = false
 
-opt.hidden = true
+-- visual
+vim.o.termguicolors = true
+vim.o.showmatch = true
+vim.o.showmode = false
+vim.o.pumheight = 10
+vim.o.pumblend = 0
+vim.o.winblend = 0
+
+-- behavior
+vim.o.hidden = true
+vim.o.errorbells = false
+vim.o.mouse = "a"
+
+-- clipboard
+vim.o.clipboard = 'unnamedplus'
+vim.g.clipboard = {
+  name = 'win32yank-wsl',
+  copy = {
+    ['+'] = 'win32yank.exe -i --crlf',
+    ['*'] = 'win32yank.exe -i --crlf',
+  },
+  paste = {
+    ['+'] = 'win32yank.exe -o --lf',
+    ['*'] = 'win32yank.exe -o --lf',
+  },
+  cache_enabled = 0,
+}
